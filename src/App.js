f6da5570a3c8efc9ba43c9d907e0451a23efeb3f
thorsush <thoratsushil703.com>
2021-12-26 12:21:02 +0530

    nearly done

--- src/App.js
+++ src/App.js
@@ -6,22 +6,6 @@ import Calcibox from './Calcibox';
 function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Blah! Blah! Blah! 
-        </a>
-      </header>
-    </div>
     <Calcibox/>
     </>
   );
